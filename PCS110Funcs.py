4d9211884c6d49c69e7d363309d455a14eabfaef
Shmath64 <64mdavis@gmail.com>
2023-11-15 21:57:56 -0500

    Added constants, and a function for checking types

--- PCS110Funcs.py
+++ PCS110Funcs.py
@@ -6,13 +6,40 @@ Created on Wed Nov 15 08:05:04 2023
 """
 #PCS110 Functions and Constants
 import math
-nTypes = [float, int]
+
+#CONSTANTS:  (!!Maybe read/write this from a text file?)
+nTypes = (float, int) #Numerical types; used for checking data types
+c = 3e8         #m/s - speed of light
+G = 6.7e-11     #N*m**2 - Gravitational Constant
+g = 9.8         #N/kg - Approx. gravitational field near Earth's surface
+me = 9e-31      #kg - Electron mass
+mp = 1.7e-27    #kg - Proton mass
+mn = 1.7e-27    #kg - Neutron mass
+EC = 9e9        #N*m**2/C**2 - Electric constant
+e = 1.6e-19     #C - Proton Charge
+Na = 6.02e23    #Atoms/mol - Avogadro's Number
+h = 6.6e-34     #J*s - Plank's Constant
+
+def checkNType(x):
+    if type(x) not in nTypes:
+        raise TypeError("must be a numerical value (float or integer)")
+
+def checkNTypeTuple(t):
+    for x in t:
+        if type(x) not in nTypes:
+            raise TypeError("The tuple must only contain numerical values (floats or integers)")
+            
+def gamma(v): #Lorentz Factor
+    checkNType(v)
+    return 1 / math.sqrt(1-(v**2 / c**2))
 
 class Vector:
     
     def __init__(self, x=0, y=0, z=0):
         #Can take a 3-tuple, or 3 variables
         if type(x) == tuple: 
+            checkNTypeTuple(x)
+            if len(x) != 3: raise Exception("The Tuple must have 3 elements")
             self.x, self.y, self.z = x
         elif type(x) in nTypes and type(y) in nTypes and type(z) in nTypes:
             self.x = x
@@ -41,5 +68,10 @@ class Vector:
         #self.y = math.cos(thetay)
         #self.z = math.cos(thetaz)
         
+
+if __name__ == "__main__":
+    running = True
+    print("PCS110 Functions and Constants")
+    #while loop with inputs and checking things and stuff
     
         
