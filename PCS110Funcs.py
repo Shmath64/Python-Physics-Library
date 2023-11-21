0d16061ecb6d66916be57af4b381a8774e5da2bf
Shmath64 <64mdavis@gmail.com>
2023-11-21 08:11:05 -0500

    Added functionality of "constantsFile"

--- PCS110Funcs.py
+++ PCS110Funcs.py
@@ -8,7 +8,7 @@ Created on Wed Nov 15 08:05:04 2023
 import math
 from typing import Union
 
-#CONSTANTS:  (!!Maybe read/write this from a text file?)
+#CONSTANTS (default values - can be overwritten by constants.txt)
 nTypes = (float, int) #Numerical types; used for checking data types
 c = 3e8         #m/s - speed of light
 G = 6.7e-11     #N*m**2 - Gravitational Constant
@@ -20,6 +20,8 @@ EC = 9e9        #N*m**2/C**2 - Electric constant
 e = 1.6e-19     #C - Proton Charge
 Na = 6.02e23    #Atoms/mol - Avogadro's Number
 h = 6.6e-34     #J*s - Plank's Constant
+relativisticThreshold = 0.1 #At what velocity should relativistic effects be considered (in terms of c)
+defaultConstants ={"c":c,"G":G, "g":g,"me":me,"mp":mp,"mn":mn,"EC":EC,"e":e,"Na":Na,"h":h,"relativisticThreshold":relativisticThreshold}
 
 degToRad = lambda x: x*(math.pi/180)
 radToDeg = lambda x: x*(180/math.pi)
@@ -109,7 +111,16 @@ def forceOfGravity(m1, m2, r) -> Union[Vector, float]: #Checked, Works!
     elif checkNType(r):                 #If not a vector
         return -(G * m1 * m2) / (r**2)  #Return scalar value
     
-def nearEarthForceOfGravity(m):
+def forceOfElectromagnetism(q1, q2, r) -> Union[Vector, float]:
+    if isinstance(r, Vector):                        #If it's a vector
+        fMag = -(EC * q1 * q2) / (r.magnitude()**2)   #Get magnitude of force
+        return Vector.scalarMultiply(r.unit(), fMag) #Return vector value
+    
+    elif checkNType(r):                 #If not a vector
+        return -(EC * q1 * q2) / (r**2)  #Return scalar value
+
+    
+def nearEarthForceOfGravity(m): #Simply mg for near-Earth approximations
     checkNType(m)
     return m * g
     
@@ -161,32 +172,44 @@ def maxForceOfFriction(mu, Fn):
     checkNType(mu, Fn)
     return mu * Fn #Product of coefficent of friction and the normal force
 
-def youngsModulus(Ft, A, dL, Lr):
+def youngsModulus(Ft, A, dL, Lr): #Checked, Works!
     """
     
 
     Parameters
     ----------
-    Ft : Force of Tension
+    Ft : Force of Tension (N)
         DESCRIPTION.
-    A : Area
+    A : Area (m**2)
         DESCRIPTION.
-    dL : Stretch
+    dL : Stretch (m)
         DESCRIPTION.
-    Lr : Relaxed Length
+    Lr : Relaxed Length (m)
         DESCRIPTION.
 
     Returns
     -------
-    Float
-        Young's Modulus.
+    Float (N/m**2) or Pa
+        Young's Modulus;
+        The "stretchiness" of a material regardless of size/shape
+        Always a pull force; "You can't push a rope"
 
     """
     
     checkNType(Ft, A, dL, Lr)
-    return (Ft / A) / (dL/ Lr)
+    return (Ft / A) / (dL / Lr) #Formula: stress (Ft/A) / strain (dL/Lr)
     
-    
+def restEnergy(m): #mc**2
+    checkNType(m)
+    return m * (c**2)
+
+def kineticEnergy(v, m): #gamma(mc**2) - mc**2 OR 1/2 mv**2
+    checkNType(v, m)
+    #The "relativisticThreshold" determines which equation for kinetic energy should be used
+    if v >= relativisticThreshold*c:
+        return gamma(v)*restEnergy(m) - restEnergy(m)
+    else:
+        return 0.5 * m * (v**2)
 
 
 """
@@ -204,4 +227,31 @@ if __name__ == "__main__":
             print("Not a valid Input")
 """
     
+def restoreDefaultConstants():
+    open('constantsFile.txt', 'w').close() #Clears content of file
+    file = open("constantsFile.txt", "w")
+    for c in defaultConstants: #Writes each constant to file
+        file.write(f"{c} : {defaultConstants[c]}\n")
+        
+    file.close()
+    f = open("constantsFile.txt", "r")
+    updateConstantsFromFile(f)
+        
+def updateConstantsFromFile(file):
+    fLines = file.readlines()
+    for line in fLines: #For each line
+        item = line.split()[0] 
+        value = line.split()[2]
+        print(item, value)
+        globals()[item] = float(value) #Set the variable of the line to the value of the line.
+
+if __name__ == "__main__":
+    try:
+        f = open("constantsFile.txt", "r")
+        updateConstantsFromFile(f)
+    except FileNotFoundError:
+        f = open("constantsFile.txt", "w")
+        restoreDefaultConstants()
+    f.close()
+        
         
